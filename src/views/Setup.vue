d6e71d4f0faa2d200b0ec096ec481b1ac2c62af4
kazuki.takeyama <kazuki.takeyama@rakuten.cp>
2019-01-12 17:14:13 +0900

    first commit

---
+++ src/views/Setup.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+        <TGLS msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import HelloWorld from "@/components/HelloWorld.vue";
+    import TGLS from "@/components/TGLS.vue"
+
+    export default {
+        name: "tgls",
+        components: {
+            TGLS
+        }
+    };
+</script>
